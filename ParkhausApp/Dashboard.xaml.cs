2efde97bdb77062f7432e44d9468b94041ad1d90
DerDonDevrim <devrim.ceylan@stud.edubs.ch>
2026-02-10 21:07:15 +0100

    current parkingtime timer added

--- ParkhausApp/Dashboard.xaml.cs
+++ ParkhausApp/Dashboard.xaml.cs
@@ -6,5 +6,21 @@ public partial class Dashboard : ContentPage
     public Dashboard(Ticket ticket)
 	{
 		InitializeComponent();
+		_ticket = ticket;
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		CurrentTime();
+    }
+	public void CurrentTime()
+	{
+		Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+		{
+			var duration = DateTime.Now - _ticket.entryTime;
+			parkduration.Text = $"Aktuellezeit: {duration:hh\\:mm\\:ss}";
+			return true;
+		});
 	}
 }
