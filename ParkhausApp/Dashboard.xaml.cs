557e0071ef8bab059ff367ef1bf9ca589404b562
DerDonDevrim <devrim.ceylan@stud.edubs.ch>
2026-02-11 19:30:28 +0100

    Current price added

--- ParkhausApp/Dashboard.xaml.cs
+++ ParkhausApp/Dashboard.xaml.cs
@@ -2,17 +2,19 @@ namespace ParkhausApp;
 
 public partial class Dashboard : ContentPage
 {
-    private Ticket _ticket;
-    public Dashboard(Ticket ticket)
+	private Ticket _ticket;
+	public Dashboard(Ticket ticket)
 	{
 		InitializeComponent();
 		_ticket = ticket;
 	}
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
 		CurrentTime();
+		CurrentPrice();
+
     }
 	public void CurrentTime()
 	{
@@ -23,4 +25,14 @@ public partial class Dashboard : ContentPage
 			return true;
 		});
 	}
-}
+	public void CurrentPrice() {
+		Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+		{
+			var time = DateTime.Now - _ticket.entryTime;
+			var timeMinute = Math.Floor(time.TotalMinutes);
+			double price = timeMinute / 1 * 2;
+            parkprice.Text = $"Aktueller Preis: {price} CHF";
+            return true;
+
+		});
+} }
